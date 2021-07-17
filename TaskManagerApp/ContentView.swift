9c015f903072e209ea0ae7d698ea1ba7ddddd262
Ari Supriatna <ari.supriatna@bibit.id>
2021-07-17 21:26:50 +0700

    Initial Commit

---
+++ TaskManagerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TaskManagerApp
+//
+//  Created by Ari Supriatna on 17/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TaskManagerApp/TaskManagerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerAppApp.swift
+//  TaskManagerApp
+//
+//  Created by Ari Supriatna on 17/07/21.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
